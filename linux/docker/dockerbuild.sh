037e9e8999224e2884e8a3e62973f77a1fec7e94
zhangpengfei <zpf12345678910@gmail.com>
2021-05-20 13:59:18 +0800

    脚本备份

---
+++ linux/docker/dockerbuild.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+#!/bin/sh
+# sh /opt/shell/dockerbuild.sh app-demo-1.0.0  /opt/package/ 18001 app-demo test
+echo "===============开始构建镜像并上传到镜像仓库===================="
+
+serverID=$1
+packagepath=$2
+port=$3
+
+appId=$4
+appVersion=$5
+
+dockerfilepath=${packagepath}/dockerfile
+echo "开始部署构建${serverID}镜像"
+
+# dockerfile 文件
+`cd ${dockerfilepath}`
+`touch ${serverID}`
+dockerfile=${dockerfilepath}/${serverID}
+
+#安装包
+deployzipfile=${packagepath}/${serverID}.jar
+echo "安装包为:$deployzipfile"
+
+# 生成dockerFile文件
+#echo "FROM base-1.2" >${dockerfile}
+echo "FROM openjdk:8-jdk-alpine" >${dockerfile}
+#echo "FROM openjdk:8-jdk-oracle" >${dockerfile}
+echo "MAINTAINER xxx@gmail.com>" >>${dockerfile}
+echo "RUN mkdir /var/jarDir" >>${dockerfile}
+echo "ADD ${serverID}.jar /var/jarDir/${serverID}.jar" >>${dockerfile}
+#echo "ADD arthas-packaging-3.1.7-bin.tar.gz /var/jarDir/" >>${dockerfile}
+echo "COPY --from=hengyunabc/arthas:latest /opt/arthas /opt/arthas" >> ${dockerfile}
+echo "RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime" >>${dockerfile}
+echo "RUN echo 'Asia/Shanghai' >/etc/timezone" >>${dockerfile}
+echo "RUN apk add --no-cache tini" >>${dockerfile}
+echo "EXPOSE ${port}" >>${dockerfile}
+echo "ENTRYPOINT [\"/sbin/tini\", \"--\"]" >>${dockerfile}
+echo "CMD [\"sh\",\"-c\",\"java -Duser.timezone=GMT+08 -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar /var/jarDir/${serverID}.jar \"] ">>${dockerfile}
+#echo "ENTRYPOINT exec java -Duser.timezone=GMT+08  -Xms1500m -Xmx1500M -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar /var/jarDir/${serverID}.jar  ">>${dockerfile}
+#echo "CMD ["sh","-c","java -Xms1500m -Xmx1500M -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar /usr/local/factoring_web/xxxx.jar"] ">>${dockerfile}
+#echo "ENTRYPOINT exec java -Duser.timezone=GMT+08  -jar /var/jarDir/${serverID}.jar" >>${dockerfile}
+
+Flag=`docker images | grep ${serverID} | wc -l`
+ # 重启前检查容器是否存在，
+if [ $Flag -eq 1 ];
+ then
+    echo ""
+    echo "删除${serverID}镜像"
+    docker rmi -f ${serverID}
+    echo ""
+    echo "删除${serverID}镜像成功"
+fi
+
+
+if [ -f $deployzipfile ]
+then
+    if [ -f $dockerfile ];
+    then
+        echo ""
+            echo " dockerfile 文件已经生成"
+            cat $dockerfile
+            sleep 1
+        echo ""
+            echo "生成docker镜像文件"
+            cd ${packagepath}
+
+        pwd
+        echo ""
+            docker build -f ${dockerfile} -t ${serverID} .
+        echo ""
+
+        imagesFlag=`docker images ${serverID} | wc -l`
+        # 判断应用镜像是否生成
+        if [ $imagesFlag -eq 2 ];
+        then
+            echo "应用镜像已经生成"
+            echo ""
+            echo "开始上传${serverID}镜像到私有Harbor仓库"
+            echo ""
+            echo "登陆仓库"
+            docker login dockerhub.com -u xxx -p 'xxx'
+            echo ""
+            echo "打Tag版本"
+            docker tag ${serverID}  dockerhub.com/demo/${appId}:${appVersion}
+            echo ""
+            echo "推送openapp.vsbclub.com/openapp/${appId}:${appVersion}镜像"
+            docker push dockerhub.com/demo/${appId}:${appVersion}
+            echo ""
+	    echo ""
+	    echo "删除${serverID}镜像"
+	    docker rmi -f ${serverID}
+	    echo ""
+	    echo "删除${serverID}镜像成功"
+        else
+            echo "${serverID}应用镜像生成失败"
+        fi
+    else
+        echo "${serverID} 的 DockerFile 文件未生成"
+    fi
+else
+	echo "${serverID}部署包不存在，请检查!"
+fi
+
+
--- win/start.bat
+++ win/start.bat
@@ -19,3 +19,48 @@ echo javaw START
 start /d  "E:\tools\nginx-1.14.2\" nginx.exe  -c conf/nginx.conf
 
 exit
+
+echo pg_sql START
+"E:\Program Files\VSBEdu\pgsql\bin\pg_ctl.exe"  -D "E:\Program Files\VSBEdu\pgsql\data" start
+
+echo idea START
+start /d "E:\Program Files\JetBrains\IntelliJ IDEA 2019.1.2\bin" idea64.exe
+
+echo VS CODE START
+start /d "E:\tools\Microsoft VS Code" code.exe
+
+echo sublime START
+start /d "E:\Program Files\Sublime Text 3" sublime_text.exe
+
+echo Chrome START
+start /d "C:\Program Files (x86)\Google\Chrome\Application" chrome.exe
+
+echo cloudmusic START
+start /d "E:\Program Files (x86)\Netease\CloudMusic" cloudmusic.exe
+
+
+echo javaw START
+rem start /d  "E:\workspace\idea\app-cloud\" taskrunjavaw.bat
+
+echo nginx START
+start /d  "E:\tools\nginx-1.14.2\" nginx.exe  -c conf/nginx.conf
+
+echo dbeaver START
+start /d  "E:\tools\dbeaver-ee-7.2.0-win32.win32.x86_64\dbeaver" dbeaver.exe
+
+echo dbeaver START
+start /d  "E:\tools\Redis-x64-3.2.100\" redis-server.exe --service-install redis.windows-service.conf --loglevel verbose
+
+echo vmware START
+rem "E:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" start "E:\Users\Administrator\Documents\Virtual Machines\CentOS-192.168.160.75\CentOS-192.168.160.75.vmx"
+
+echo ES START
+start /d "E:\tools\elk\elasticsearch-7.5.1-master\bin"  elasticsearch.bat
+
+start /d "E:\tools\elk\elasticsearch-7.5.1-slave1\bin"  elasticsearch.bat
+
+start /d "E:\tools\elk\kibana-7.5.1-windows-x86_64\bin"  kibana.bat
+
+echo jupyter START
+start  E:\ProgramData\Anaconda3\python.exe E:\ProgramData\Anaconda3\cwp.py E:\ProgramData\Anaconda3\envs\tensorflow E:\ProgramData\Anaconda3\envs\tensorflow\python.exe E:\ProgramData\Anaconda3\envs\tensorflow\Scripts\jupyter-notebook-script.py E:\workspace\git
+pause
